83e6440d5636c47617bec3e3b7a0f1b1af5f6770
alihasnainramzan <167064865+alihasnainramzan@users.noreply.github.com>
2025-04-15 23:41:27 +0500

    Update staticCredential.js

--- src/utils/staticCredential.js
+++ src/utils/staticCredential.js
@@ -1,5 +1,5 @@
 export const google_client_id =
-  "";
+  "10428493073-bnts0v0afg5ndf54rpvbk655sdp48hb2.apps.googleusercontent.com";
 export const fb_app_id = "";
 export const appleLoginCredential = {
   serviceId: "",
